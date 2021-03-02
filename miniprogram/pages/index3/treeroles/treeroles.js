c707cd54c0a656922e16e07c09a51274b555c4ad
hrx <2320272622@qq.com>
2021-03-02 09:15:16 +0800

    完成了个人中心里面的删除我的树洞功能

--- miniprogram/pages/index3/treeroles/treeroles.js
+++ miniprogram/pages/index3/treeroles/treeroles.js
@@ -6,7 +6,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    theHostOpenId: "oRKwI5j_JXmNzEkwHVj5IoLmgHZc" // 未来可换成登录者的openid，目前先用我的openid(——hrx)
+    theHostOpenId: "oRKwI5j_JXmNzEkwHVj5IoLmgHZc",  // 未来可换成登录者的openid，目前先用我的openid(——hrx)
+    tempid: "" // 临时用来储存要删除的id
   },
 
   /**
@@ -94,5 +95,35 @@ Page({
     }).catch(err => {
       console.log(err)
     })
+  },
+
+  /**
+   * 处理删除树洞事件
+   */
+  deleteMe(res){
+    console.log("用户选中树洞名: ", res.currentTarget.dataset.thistreeholeid);
+    this.setData({
+      tempid : res.currentTarget.dataset.thistreeholeid,
+    });
+    const that = this;
+    // 提醒用户是否要删除树洞
+    wx.showModal({
+      title: '',
+      content: '确定要删除吗？',
+      success: function (e) {
+        // 点击了确定以后会删除树洞
+        if (e.confirm) { 
+          console.log('用户点击确定')
+          db.collection('index2_treeholes')
+          .doc(that.data.tempid).remove()
+          console.log('成功删除树洞: ', that.data.tempid)
+          // 显示删除的提示界面
+          that.onReady() // 删除后重新渲染界面
+          wx.showToast({
+            title: '删除成功',
+          })
+        } // ene if(e.confirm)
+      } // end function success
+    }) // end wx.showModal
   }
 })

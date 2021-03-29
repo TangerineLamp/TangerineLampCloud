cb2c89fc21c5a02194d131b3e22e50918b87b4e0
hrx <2320272622@qq.com>
2021-03-29 09:47:00 +0800

    SCL-90的外壳
    做了SCL-90的外壳。具体内容之后再加

---
+++ miniprogram/pages/index1/test1/testlists/Scl-90/Scl-90.js
@@ -0,0 +1,72 @@
+// pages/index1/test1/testlists/Scl-90/Scl-90.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    selectButton: [
+      {value: 1, name: '没有'},
+      {value: 2, name: '很轻'},
+      {value: 3, name: '中等'},
+      {value: 4, name: '偏重'},
+      {value: 5, name: '严重'},
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})

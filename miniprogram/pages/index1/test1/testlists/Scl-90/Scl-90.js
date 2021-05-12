6a9d13c200a24209716d51d71df074e5b90a3aa6
hrx <2320272622@qq.com>
2021-05-12 14:11:21 +0800

    Scl-90自评
    自评和总分功能已经写好了，但是还没有对总分进行判断。而且ui界面还比较丑

--- miniprogram/pages/index1/test1/testlists/Scl-90/Scl-90.js
+++ miniprogram/pages/index1/test1/testlists/Scl-90/Scl-90.js
@@ -1,72 +1,140 @@
-// pages/index1/test1/testlists/Scl-90/Scl-90.js
+const db = wx.cloud.database();
+var maxTopicCount = 0;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    selectButton: [
+    selectButton:                // 选项类型列表，存放问题选项
+    [
       {value: 1, name: '没有'},
       {value: 2, name: '很轻'},
       {value: 3, name: '中等'},
       {value: 4, name: '偏重'},
       {value: 5, name: '严重'},
-    ]
+    ],
+    topicData: [],               // 问题列表，存放问题，需要下拉菜单更新
+    isShowResult: false,         // 判断是否显示结果
+    isShowSubmit: false,         // 提交按钮，一开始不会显示，只有问题刷新完才会显示
+    totalGrade: 0                // 最终的得分
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 生命周期函数--监听页面初次加载完成
    */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    wx.showLoading({
+      title: '玩命加载中',
+    })
+    this.getInitScl90Test()
+    this.getMaxCount()
+    wx.hideLoading()
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 获取题目的总数
    */
-  onReady: function () {
-
+  getMaxCount(){
+    db.collection("index1_Scl90TestList")
+    .count()
+    .then(res=>{
+      maxTopicCount = res.total
+    })
   },
 
   /**
-   * 生命周期函数--监听页面显示
+   * 初始化内容
+   * 只能获得最多20个
    */
-  onShow: function () {
-
+  getInitScl90Test(){
+    db.collection("index1_Scl90TestList").get()
+    .then(res => {
+      console.log(res)
+      this.setData({
+        topicData: res.data
+      })
+    })
   },
 
   /**
-   * 生命周期函数--监听页面隐藏
+   * 只要触底就进行更新
+   * 直至将collection中的问题条目更新完
+   * 该内容没有设置limit，一次最高可以更新20个
    */
-  onHide: function () {
-
+  onReachBottom: function(){
+    let oldData = this.data.topicData;
+    // 如果现在问题的数量小于问题总数量就下拉更新
+    console.log(oldData.length)
+    console.log(oldData)
+    if(oldData.length < maxTopicCount){
+      // 显示加载条
+      wx.showLoading({
+        title: '加载中',
+      })
+      // 开始更新下拉的数据
+      db.collection("index1_Scl90TestList")
+      .skip(oldData.length)
+      .get()
+      .then(res=>{
+        // 将新问题进行缝合
+        let newList = res.data
+        let newData = oldData.concat(newList)
+        // 缝合好的新老数据传给data中问题列表
+        this.setData({
+          topicData: newData
+        })
+      })
+      // 加载条关闭
+      wx.hideLoading()
+    }
+    // 如果现在问题的数量等于问题总数量就显示‘加载完毕’
+    else{
+      this.setData({
+        isShowSubmit: true
+      })
+      wx.showToast({
+        title: '到底了哦',
+        icon: 'success',
+        duration: 1000
+      })
+    }
   },
 
   /**
-   * 生命周期函数--监听页面卸载
+   * 对radio-group改变事件的处理
+   * 可以用于对每个问题的每个选项的值进行捕获
    */
-  onUnload: function () {
-
+  radioChange(e){
+    let value = Number(e.detail.value)
+    console.log(value)
+    this.data.topicData[parseInt(value / 10) - 1]["gradeValue"] = value % 10
+    // console.log(this.data.topicData)
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  toSubmit(e){
+    const dataTemp = this.data.topicData
+    let res = 0
+    for (let i=0;i<dataTemp.length;++i){
+      // 如果有未填的部分则告知用户哪里未填
+      if (dataTemp[i]["gradeValue"] == ""){
+        wx.showToast({
+          title: "第"+(i+1)+"题还没有填哦",
+          icon: 'none',
+          duration: 1000
+        })
+        return
+      }
+      // 如果都填了的话就计算总分
+      else {
+        res += dataTemp[i]["gradeValue"]
+      }
+    }
+    this.setData({
+      totalGrade: res,
+      isShowResult: true
+    })
+    console.log(this.data.totalGrade)
+    console.log(this.data.isShowResult)
   }
 })
